25b6cba7cc3ae176dfa4139361cfdd04c84df4c3
EmberTSeal <trishaseal49@gmail.com>
2023-06-20 14:29:59 +0530

    Add click input to cards

--- script.js
+++ script.js
@@ -7,62 +7,77 @@
 let playerScore = 0;
 let computerScore = 0;
 
-game();
+const playerRock = document.querySelector('.rock');
+const playerPaper = document.querySelector('.paper');
+const playerScissors = document.querySelector('.scissors');
+
+let playerSelection;
+
+playerRock.addEventListener('click', () => {
+    playerSelection = 'rock';
+    game();
+});
+playerPaper.addEventListener('click', () => {
+    playerSelection = 'paper';
+    game();
+});
+playerScissors.addEventListener('click', () => {
+    playerSelection = 'scissors';
+    game();
+});
+
 
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3 + 1);
     if (choice === 1)
-        return 'rock'
+        return 'rock';
     else if (choice === 2)
-        return 'paper'
+        return 'paper';
     else
-        return 'scissors'
+        return 'scissors';
 }
 
 function gameRound(playerSelection, computerSelection) {
     let player = playerSelection.toLowerCase();
-    if (player === computerSelection){
-        playerScore +=1;
-        computerScore +=1;
-        return('Tie!');
+    if (player === computerSelection) {
+        playerScore += 1;
+        computerScore += 1;
+        return ('Tie!');
     }
     else {
         let result = roundResult(player, computerSelection);
         if ((player === 'rock' && computerSelection === 'paper') ||
-            (player === 'scissors' && computerSelection === 'rock')||
+            (player === 'scissors' && computerSelection === 'rock') ||
             (player === 'paper' && computerSelection === 'scissors')) {
-                computerScore +=1;
-                return ('You Lose! ' + result);
-            }
-        else
-            playerScore +=1;
-            return ('You Win! ' + result);    
+            computerScore += 1;
+            return ('You Lose! ' + result);
         }
+        else
+            playerScore += 1;
+        return ('You Win! ' + result);
+    }
 
 }
 
-function roundResult(inputOne, inputTwo){
-    if((inputOne === 'rock' && inputTwo === 'paper') || (inputOne === 'paper' && inputTwo === 'rock'))
-        return('Paper beats Rock.');
-    else if((inputOne === 'rock' && inputTwo === 'scissors') || (inputOne === 'scissors' && inputTwo === 'rock'))
-        return('Rock beats Scissors.');
+function roundResult(inputOne, inputTwo) {
+    if ((inputOne === 'rock' && inputTwo === 'paper') || (inputOne === 'paper' && inputTwo === 'rock'))
+        return ('Paper beats Rock.');
+    else if ((inputOne === 'rock' && inputTwo === 'scissors') || (inputOne === 'scissors' && inputTwo === 'rock'))
+        return ('Rock beats Scissors.');
     else
-        return('Scissors beats Paper.');
+        return ('Scissors beats Paper.');
 }
 
-function game(){
-    for(let i = 1; i<=5; i++){
-        let playerSelection =  prompt('Your choice? ');
-        let computerSelection = getComputerChoice();        
+function game() {
+        let computerSelection = getComputerChoice();
         console.log(`Your input: ${playerSelection}`);
         console.log(`Computer input: ${computerSelection}`);
         console.log(gameRound(playerSelection, computerSelection));
         console.log(`Player Score: ${playerScore} , Computer Score: ${computerScore}`);
-    }
-    if(playerScore > computerScore)
-        console.log('YOU WIN!' )
-    else if(playerScore < computerScore)
-        console.log('YOU LOSE!') 
-    else
-        console.log('TIE!');       
+    // if (playerScore > computerScore)
+    //     console.log('YOU WIN!')
+    // else if (playerScore < computerScore)
+    //     console.log('YOU LOSE!')
+    // else
+    //     console.log('TIE!');
 }
